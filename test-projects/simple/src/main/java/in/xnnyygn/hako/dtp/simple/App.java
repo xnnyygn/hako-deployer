fe8b9e0b18488f446304af0c58f7ad0ce2071b33
xnnyygn <xnnyygn@gmail.com>
2013-07-18 17:24:22 +0800

    inital import

---
+++ test-projects/simple/src/main/java/in/xnnyygn/hako/dtp/simple/App.java
@@ -0,0 +1,13 @@
+package in.xnnyygn.hako.dtp.simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
